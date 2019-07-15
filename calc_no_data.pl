b53f1a0b0954067fe3426b2ae5050383375fdb31
windhooked <hannesdewaal@gmail.com>
2019-07-15 17:02:38 +0100

    Update calc_no_data.pl

--- calc_no_data.pl
+++ calc_no_data.pl
@@ -18,39 +18,41 @@ sub sgn {
 	return ($x<=>0) ;# sgn($x)
 }
 for ( <DATA> ) {
-	chomp;
-	next if ( /null/);
-	my @l = split /\,/;
-	my $a = $l[1];
-	my $b = $l[2];
-	my $z = $l[3];
+	   chomp;
+        next if ( /null/);
+        my @l = split /\,/;
+        my $a = $l[1];
+        my $b = $l[2];
+        my $z = $l[3];
 
         my $r = bin2dec("$aa$bb$a$b"); # generate current state for lut
-	my $t = bin2dec("$bb$zz$b$z");
-	#my $t = bin2dec("$aa$zz$a$z");
+        my $t = bin2dec("$bb$zz$b$z");
+        #my $t = bin2dec("$aa$zz$a$z");
 
-	$count += $c[$r];
-	$count_Z += $c[$t];
+        $count += $c[$r];
+        $count_Z += $c[$t];
+        $Mrr = $count;
 
-	$N = 1000; #Nominal increment between two fixed reference marks in signal periods (see table below)
-	$R =  (2*$Mrr)-$N;
-	$D = +1; #direction
+        $N = 1000; #Nominal increment between two fixed reference marks in signal periods (see table below)
+        $B =  (2*$Mrr)-$N;
+        $D = +1; #direction
 
-	$P1 = (abs($R) - sgn($R) -1) * $N/2 + ( sgn($R) - sgn($D) ) * abs( $Mrr) /2;
+        $P1 = (abs($B) - sgn($B) -1) * $N/2 + ( sgn($B) - sgn($D) ) * abs( $Mrr) /2;
 
-	if ( $a != $aa ) {
-		$Mrr++; ## number of signal periods between reference marks
-	}
-	if ( $z != $zz ) {
-		printf "d:%10f dp1:%20f P1: %10d R:%5d %5d %10d %10d\n",$count*0.001, $P1-$PP1,$P1, $R, $count, $count - $bc, $count_Z;
-		#printf "%10f;%20f;%10d;%5d;%5d;%10d;%10d\n",$count*0.001, $P1*0.001,$P1, $R, $count, $count - $bc, $count_Z;
-	        $bc = $count;
-		$Mrr = 0;
-	}
+        if ( $a != $aa ) {
+                #       $Mrr++; ## number of signal periods between reference marks
+        }
+        if ( $z != $zz ) {
+                printf "d:%10f dP1:%20f P1:%10d B:%5d CAB:%5d d:%10d CZ:%10d \n",$count*0.001, $P1-$PP1,$P1/1000, $B, $count, ($P1/1000) + 501, $count_Z;
+                #printf "%10f;%20f;%10d;%5d;%5d;%10d;%10d\n",$count*0.001, $P1*0.001,$P1, $R, $count, $count - $bc, $count_Z;
+                $bc = $count;
+                $Mrr = 0;
+        }
 
-	$bb = $b;
-	$aa = $a;
-	$zz = $z;
-	$PP1 = $P1;
+        $bb = $b;
+        $aa = $a;
+        $zz = $z;
+        $PP1 = $P1;
+	
 }
 #
